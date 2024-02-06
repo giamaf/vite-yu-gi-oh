54e9fee69a2a7bf1804f1d2d358ba19f18ee55ef
giamaf <gianluca.maffucci@hotmail.com>
2024-02-06 16:12:28 +0100

    get item's types from API

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,43 @@
 import axios from 'axios';
 import { store } from './assets/data/store.js';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+const secondEndopoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons/types1';
 import AppHeader from './AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 export default {
   // Logica Javascript
   name: 'Pokévuex',
 
+  data: () => ({
+    store
+  }),
+
   components: { AppHeader, AppMain },
 
-  created() {
-    axios.get(endpoint).then(res => {
-      //Inserisco la risposta dell'API nello STORE 
-      store.pokemons = res.data.docs;
-    })
+  methods: {
+    fetchPokemons(data) {
+      axios.get(data).then(res => {
+        //Inserisco la risposta dell'API nello STORE 
+        store.pokemons = res.data.docs;
+        // store.types = res.data;
+        // console.log(store.types)
+      })
+    },
+
+    fetchTypes(data) {
+      axios.get(data).then(res => {
+
+        store.types = res.data;
+
+      })
+    },
   },
+
+  created() {
+    this.fetchPokemons(endpoint);
+    this.fetchTypes(secondEndopoint);
+  },
+
 };
 </script>
  
